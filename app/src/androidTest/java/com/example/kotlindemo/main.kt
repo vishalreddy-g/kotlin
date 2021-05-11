be499dfbb81ca9a5956f57872d22ca1cc5c7f780
G VISHAL REDDY <vishalreddy12383@gmail.com>
2021-05-11 09:51:06 -0700

    my first commit

--- app/src/androidTest/java/com/example/kotlindemo/main.kt
+++ app/src/androidTest/java/com/example/kotlindemo/main.kt
@@ -1,5 +1,10 @@
 package com.example.kotlindemo
 
 fun main(){
-    print("Hello kotlin")
+    var myString:String="Android Kotlin"
+    var myfloat:Float=13.37F
+    var myChar:Char='F'
+    var ffloat:Double=3.14159265358979
+    var myInt:Int=25
+    print("$myString\n$myfloat$myChar\n$ffloat\n$myInt")
 }
